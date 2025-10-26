fd19be1cdca477ec014076829c64a7dc1dbdfc3c
suvrajit <suvrajit.arowana@gmail.com>
2025-10-26 22:55:34 +0530

    updated trust card

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # main.py
 
 from fastapi import FastAPI
-from app.routes import upload, match, analyze_and_match, analyze_and_match_html
+from app.routes import upload, match, analyze_and_match, analyze_and_match_html, trust_card
 
 app = FastAPI(title="ProviderGPT AI")
 
@@ -10,3 +10,4 @@ app.include_router(upload.router)
 app.include_router(match.router)
 app.include_router(analyze_and_match.router)
 app.include_router(analyze_and_match_html.router)
+app.include_router(trust_card.router)
